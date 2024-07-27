f94b2562c6e8e1e44f5e7cb6884fc45c8e208414
DaniTech <tochidan11@gmail.com>
2024-07-27 03:28:06 -0700

    Done with changeNetwork and  handleNetworkSwitch functionss

--- Context/constants.js
+++ Context/constants.js
@@ -1,2 +1,91 @@
 import { ethers } from "ethers";
 import web3Modal from "web3modal";
+
+import ERC20Generator from "./ERC20Generator.json";
+import icoMarketplace from "./icoMarketplace.json";
+
+export const ERC20Generator_ABi = ERC20Generator.abi;
+export const ERC20Generator_BYTECODE = ERC20Generator.bytecode;
+
+export const ICO_MARKETPLACE_ADDRESS =
+  process.env.NEXT_PUBLIC_ICO_MARKETPLACE_ADDRESS;
+
+export const ICO_MARKETPLACE_ABI = icoMarketplace.abi;
+
+//PINATA KEY
+export const PINATA_API_KEY = process.env.NEXT_PUBLIC_PINATA_AIP_KEY;
+export const PINATA_API_SECRET = process.env.NEXT_PUBLIC_PINATA_SECRECT_KEY;
+
+//NETWORKS
+//configuring networks such that if users don't have them in the metamask networkss it
+//will authomatically add up
+
+const networks = {
+  polygon_amoy: {
+    chainID: `0x${Number(80002).toString(16)}`, //to string of 16 bytes
+    chainName: "Polygon Amoy",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc-amoy.polygon.technology/"],
+    blockExplorerUrls: ["https://www.oklink.com/amoy"],
+  },
+
+  polygon: {
+    chainID: `0x${Number(137).toString(16)}`, //to string of 16 bytes
+    chainName: "Polygon Mainnet",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.ankr.com/polygon"],
+    blockExplorerUrls: ["https://polygonscan.com/"],
+  },
+  bsc: {
+    chainID: `0x${Number(56).toString(16)}`, //to string of 16 bytes
+    chainName: "Binance Mainnet",
+    nativeCurrency: {
+      name: "Binance Mainnet",
+      symbol: "BNB",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.ankr.com/bsc"],
+    blockExplorerUrls: ["https://bscscan.com/"],
+  },
+  bsc: {
+    chainID: `0x${Number(8453).toString(16)}`, //to string of 16 bytes
+    chainName: "Base Mainnet",
+    nativeCurrency: {
+      name: "ETH",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: ["https://mainnet.base.org"],
+    blockExplorerUrls: ["https://bscscan.com/"],
+  },
+};
+
+const changeNetwork = async ({ networkName }) => {
+  try {
+    if (!window.ethereum) throw new Error("No crypto wallet found");
+
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          ...networkName[networkName],
+        },
+      ],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const handleNetworkSwitch = async () => {
+  const networkName = "polygon_amoy";
+  await changeNetwork(networkName);
+};
